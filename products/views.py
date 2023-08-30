7f7016e6acce434f3184a9f4305d85f2ea5fb585
omar <omarhusseinelhorbity@gmail.com>
2023-08-30 17:01:00 +0300

    adding products

--- products/views.py
+++ products/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
+from .models import Product
 def home(request):
     return render(request, 'products/home.html')
 
 def products(request):
-    return render(request, 'products/products.html')
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'products/products.html', context)
